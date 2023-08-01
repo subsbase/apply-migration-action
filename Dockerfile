6fffebb097bc216c44532f35121d2271eb346c5a
MohammedEmad <mohammed.emad@subsbase.com>
2023-08-01 16:15:12 +0300

    Initial Version of the action

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.10
+FROM mcr.microsoft.com/dotnet/sdk:6.0-alpine
 
 COPY LICENSE README.md /
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,17 @@
 #!/bin/sh -l
+project_name=$1
+db_name=$2
+db_host=$3
+db_user=$4
+db_password=$5
+db_port=$6
 
-echo "hello $1"
+echo $(ls /github/workspace/ | grep "Services")
+
+dotnet tool install --global dotnet-ef
+
+dotnet ef database update \
+--project /github/workspace/Services/$project_name/SubsBase.$project_name.Data/SubsBase.$project_name.Data.csproj \
+--startup-project /github/workspace/Services/$project_name/SubsBase.$project_name.API/SubsBase.$project_name.API.csproj \
+--context $project_nameContext \
+--connection "Host=$db_host;Port=$db_port;Database=$db_name;Username=$db_user;Password=$db_password"
